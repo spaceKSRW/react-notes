d77790043a626dcc0308b8678ac5c5adf951fc97
Karan Singh Rawat <karansinghrawat@192.168.1.71>
2023-11-03 13:43:48 +0530

    4th commit

--- 01basicreact/src/index.js
+++ 01basicreact/src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+const user="my name is karan rawat";
+const reactElement=React.createElement(
+  'a',
+  {href:"https://google.com",target:"_blank"},
+    'click me to visit google',
+     user
+)
+ReactDOM.createRoot(document.getElementById('root')).render(reactElement);
